2a5ff526984cb358f292fb07783ebeea25f67412
naokiLifebook <okinaoki24@gmail.com>
2025-12-18 02:55:48 +0900

    145 yattayo

--- 145.binary-tree-postorder-traversal.py
+++ 145.binary-tree-postorder-traversal.py
@@ -11,8 +11,52 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
+######
+## 再帰法
+#####
+
+# class Solution:
+#     def postorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+#         res = []
+        
+#         def dfs(node):
+#             if not node:
+#                 return
+#             dfs(node.left)    # 左
+#             dfs(node.right)   # 右
+#             res.append(node.val) # 根
+            
+#         dfs(root)
+#         return res
+
+#######
+### 非再帰
+#######
+
 class Solution:
     def postorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        if not root:
+            return []
         
+        stack = [root]
+        res = []
+        
+        # ステップ1: 「根 -> 右 -> 左」の順で取り出す
+        while stack:
+            node = stack.pop()
+            res.append(node.val)
+            
+            # Preorderとは逆に、左を先に積んで右を後に積む 
+            # -> 取り出すときは「右 -> 左」の順になる
+            if node.left:
+                stack.append(node.left)
+            if node.right:
+                stack.append(node.right)
+        
+        # ステップ2: 最後にひっくり返す (根右左 -> 左右根)
+        return res[::-1]
+
+
 # @lc code=end
 

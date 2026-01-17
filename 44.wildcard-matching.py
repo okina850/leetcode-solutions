a9da401a5ce78c8916da7282cc80826287e827f5
okina850 <77141566+okina850@users.noreply.github.com>
2026-01-18 05:32:35 +0900

    hello from lifebook

---
+++ 44.wildcard-matching.py
@@ -0,0 +1,11 @@
+#
+# @lc app=leetcode id=44 lang=python3
+#
+# [44] Wildcard Matching
+#
+
+# @lc code=start
+class Solution:
+    def isMatch(self, s: str, p: str) -> bool:
+        
+# @lc code=end
